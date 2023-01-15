e68cdd5eaf33b26ee86af937f74a68359c948ab7
Max Strübing <mxstrbng@gmail.com>
2023-01-15 11:43:59 +0100

    feat: define cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Specify conf filename other than '.editorconfig'
+    #[arg(short, default_value_t = String::from(".editorconfig"))]
+    f: String,
+
+    /// Specify version (used by devs to test compatibility)
+    #[arg(short, default_value_t = String::from(""))]
+    b: String,
+
+    /// Files to find configuration for.  Can be a hyphen (-) if you want path(s) to be read from stdin.
+    #[arg(required(true))]
+    filepath: Vec<PathBuf>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("args: {:?}", args);
 }
